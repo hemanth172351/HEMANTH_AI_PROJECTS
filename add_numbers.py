c82a0bc863f98d1d487b2ec489e0d8d315f588ee
hemanth <hemanthgalla63@gmail.com>
2024-12-06 11:53:43 +0530

    Added in confirmation to every function

--- add_numbers.py
+++ add_numbers.py
@@ -1,6 +1,9 @@
 def add(a,b):
+    print("I'm in add function..")
     return a+b
 def sub(a,b):
+    print("I'm in sub function..")
     return a-b
 def mul(a,b):
+    print("I'm in mul function..")
     return a*b
