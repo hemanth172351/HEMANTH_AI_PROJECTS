451ca9dcce0858c7713685cc535feb03c26f6fab
hemanth <hemanthgalla63@gmail.com>
2024-12-11 22:56:02 +0530

    made changes in both main and arithmetic branches

--- add_numbers.py
+++ add_numbers.py
@@ -6,9 +6,15 @@ def sub(a,b):
 def mul(a,b):
     return a*b
 def div(a,b):
+    print("I'm in div function")
     return a/b
 def is_even(num):
     if num%2 == 0:
+        return True
+    else:
+        return False
+def is_odd(num):
+    if num%2 != 0:
         return True
     else:
         return False
